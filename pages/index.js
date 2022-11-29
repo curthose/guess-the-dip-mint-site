12daa59fb687d31f5114e3e56cbba933fd2ee254
curthose <cemilyurdusev@gmail.com>
2022-11-29 23:28:06 +0300

    change mint button text

--- pages/index.js
+++ pages/index.js
@@ -42,7 +42,7 @@ export default function Home() {
         disableElevation
         endIcon={<TrendingDownIcon />}
       >
-        MINT
+        MINT NOW
         </Button>
         
         <MuiNextLink
